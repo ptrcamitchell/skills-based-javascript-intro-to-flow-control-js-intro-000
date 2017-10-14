90dab922615ca863de2c4c80fcf219dfa605a5a3
ptrcamitchell <ptrcamitchell@gmail.com>
2017-10-14 20:02:50 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -28,13 +28,31 @@ function ternaryTeenager(age) {
 return (age>= 13 && age <= 19) ? "You are a teenager" : "You are not a teenager"
 }
 
-var age = (age>= 13 && age <= 19)
+
 function switchAge(age) {
-case "age":
+case "13":
   return "You are a teenager"
+  break;  
+case "14":
+  return "You are a teenager"
+  break; 
+case "15":
+  return "You are a teenager"
+  break; 
+case "16":
+  return "You are a teenager"
+  break; 
+case "17":
+  return "You are a teenager"
+  break;  
+case "18":
+  return "You are a teenager"
+  break; 
+case "19":
+  return "You are a teenager"
+  break;       
 default:
   return "You have an age"
 
 }
-  break;
-}
+
