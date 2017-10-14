1cc0f3d77339b2ecfdd4cfb9fd1c17bb0a86e204
ptrcamitchell <ptrcamitchell@gmail.com>
2017-10-14 21:10:19 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -29,7 +29,8 @@ return (age>= 13 && age <= 19) ? "You are a teenager" : "You are not a teenager"
 }
 
 var age;
-function switchAge(age) {
+
+switch(age) {
  case 13:
   return "You are a teenager"
   break;
